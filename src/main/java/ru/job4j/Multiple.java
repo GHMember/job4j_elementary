eb1e7c0da52cf6cab66daca0d95237bc6b249d1a
AlexAutoTesting <zaehor@gmail.com>
2022-08-30 09:01:10 +0300

    Внесение исправлений в задание 4.2. Заголовок в файле java

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -3,7 +3,16 @@ package ru.job4j;
 public class Multiple {
 
     public static void main(String[] args) {
-        System.out.print("1*2=2\n1*3=3\n1*4=4\n1*5=5\n1*6=6\n1*7=7\n1*8=8\n1*9=9");
+
+        System.out.print("1*2=2");
+        System.out.print("1*3=3");
+        System.out.print("1*4=4");
+        System.out.print("1*5=5");
+        System.out.print("1*6=6");
+        System.out.print("1*7=7");
+        System.out.print("1*8=8");
+        System.out.print("1*9=9");
+
     }
 
 }
