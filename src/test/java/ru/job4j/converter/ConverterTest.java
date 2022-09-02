842e3e2ba825def741e900398e705f1a599db403
AlexAutoTesting <zaehor@gmail.com>
2022-09-02 15:01:33 +0300

    Правки к заданию 1. Модульные тесты

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -15,7 +15,7 @@ public class ConverterTest {
     }
 
     @Test
-    public void whenConvert140RblThen2dot3333333Dollars() {
+    public void whenConvert140RblThen2dot3333Dollars() {
         float in = 140;
         float expected = 2.3333f;
         float out = Converter.rubleToDollar(in);
