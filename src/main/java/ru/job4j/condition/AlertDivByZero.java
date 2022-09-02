5e1cceeedad835d45ba89af3585fb540394c6a7d
AlexAutoTesting <zaehor@gmail.com>
2022-09-02 10:50:34 +0300

    Правки к заданию 4.1. Операторы ветвлений

--- src/main/java/ru/job4j/condition/AlertDivByZero.java
+++ src/main/java/ru/job4j/condition/AlertDivByZero.java
@@ -11,11 +11,10 @@ public class AlertDivByZero {
     public static void possibleDiv(int number) {
         if (number < 0) {
             System.out.println("This is negative number");
-        } else if (number == 0) {
+        }
+        if (number == 0) {
             System.out.println("Could not div by 0.");
-        } else {
-            System.out.println("This is positive number");
+
         }
     }
-
 }
