f7600ae72384d8afac1d55079e59d0cd8c22eaed
AlexAutoTesting <zaehor@gmail.com>
2022-09-28 12:11:55 +0300

    3. Перегрузить метод max для трех чисел

--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -30,4 +30,36 @@ class MaxTest {
         int expected = 2;
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    public void whenMax1To2To3Then3() {
+        int first = 1;
+        int second = 2;
+        int third = 3;
+        int result = Max.max(first, second, third);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax4To1To3To2Then4() {
+        int first = 4;
+        int second = 1;
+        int third = 3;
+        int fourth = 2;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 4;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax4To4To4To4Then4() {
+        int first = 4;
+        int second = 4;
+        int third = 4;
+        int fourth = 4;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 4;
+        assertThat(result).isEqualTo(expected);
+    }
 }
